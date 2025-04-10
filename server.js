ba46f790ce08fbb76d7c67d541d9923ffa106a85
Elvijs <elvijspodnieks@gmail.com>
2025-04-11 00:28:31 +0300

    first

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { errorHandler } = require('./middleware/errorMiddleware')
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
 const port = process.env.PORT || 5000
 const connectDB = require('./database/db')
 const cors = require("cors");
