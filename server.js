c988b4c75dbba615df896c971cc6998b0edcf397
Elvijs <elvijspodnieks@gmail.com>
2025-05-03 02:40:01 +0300

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(errorHandler)
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://tenis-frontend.vercel.app",
+        origin: process.env.URI_FRONTEND,
         methods: ["GET", "POST"],
     },
 });
